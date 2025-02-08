754a668c802a93c021fc599d52ec99ed51370876
sklim84 <32153781+sklim84@users.noreply.github.com>
2025-02-08 22:01:09 +0900

    betweenness centrality 분석 추가

--- analysis/graph/graph_dist_hist_deg.py
+++ analysis/graph/graph_dist_hist_deg.py
@@ -5,37 +5,44 @@ import dask.dataframe as dd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import pandas as pd
 
 # 전체 출력 설정
 np.set_printoptions(threshold=np.inf)
 
 start = time.time()
-file_path = '../../_datasets/combined_dataset_v0.3.parquet'
-ddf = dd.read_parquet(file_path)
-df = ddf.compute()
+# file_path = '../../_datasets/combined_dataset_v0.11.parquet'
+file_path = '../../_datasets/combined_dataset_v0.12.parquet'
+# ddf = dd.read_parquet(file_path)
+# df = ddf.compute()
+df = pd.read_parquet(file_path)
 end = time.time()
 print(f"complete to read parquet file {file_path}, {end - start:.5f} sec")
 print(df.head())
 
-stats_source_deg = df.groupby('laundering_yn')['source_deg'].describe()
-print('source_deg statistics:')
-print(stats_source_deg)
-stats_source_deg.to_csv('../_results/graph_deg_hist/source_deg_stats.csv')
+# source_col = 'source_deg'
+# target_col = 'target_deg'
+source_col = 'source_between'
+target_col = 'target_between'
 
-stats_target_deg = df.groupby('laundering_yn')['target_deg'].describe()
-print('target_deg statistics:')
-print(stats_target_deg)
-stats_target_deg.to_csv('../_results/graph_deg_hist/target_deg_stats.csv')
+stats_source_col = df.groupby('laundering_yn')[source_col].describe()
+print(f'{source_col} statistics:')
+print(stats_source_col)
+stats_source_col.to_csv(f'../_results/graph_cen_hist/{source_col}_stats.csv')
+
+stats_target_col = df.groupby('laundering_yn')[target_col].describe()
+print(f'{target_col} statistics:')
+print(stats_target_col)
+stats_target_col.to_csv(f'../_results/graph_cen_hist/{target_col}_stats.csv')
 
 # log scale
 # df['log1p_source_deg'] = np.log1p(df['source_deg'])
 # df['log1p_target_deg'] = np.log1p(df['target_deg'])
-df['log_source_deg'] = np.log(df['source_deg'])
-df['log_target_deg'] = np.log(df['target_deg'])
+df[f'log_{source_col}'] = np.log(df[source_col])
+df[f'log_{target_col}'] = np.log(df[target_col])
 
-output_dir = "../_results/graph_deg_hist"
-for column in ['log_source_deg', 'log_target_deg']:
-# for column in ['source_deg', 'target_deg']:
+output_dir = "../_results/graph_cen_hist"
+for column in [f'log_{source_col}', f'log_{target_col}']:
     start = time.time()
     # laundering_yn=0
     sns.kdeplot(
